330179d1cc752017416fe29305d656f46ed91234
Gaurav Singh <gouravsinghtalyan@gmail.com>
2022-11-27 23:44:43 +0700

    Adding Config Files and Files to access config

---
+++ src/main/scala/forex/config/ApplicationConfig.scala
@@ -0,0 +1,20 @@
+package forex.config
+
+import scala.concurrent.duration.FiniteDuration
+
+case class ApplicationConfig(
+    http: HttpConfig,
+    oneFrameConfig: OneFrameConfig
+)
+
+case class HttpConfig(
+    host: String,
+    port: Int,
+    timeout: FiniteDuration
+)
+
+case class OneFrameConfig(
+    host: String,
+    refreshInterval: FiniteDuration,
+    token: String
+    )
