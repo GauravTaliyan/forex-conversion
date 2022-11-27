8609e4449122b2176c9cfcf91752c7ce27051ec0
Gaurav Singh <gouravsinghtalyan@gmail.com>
2022-11-28 04:06:21 +0700

    remove extra line

--- src/main/scala/forex/Main.scala
+++ src/main/scala/forex/Main.scala
@@ -13,7 +13,6 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     new Application[IO].stream(executionContext).compile.drain.as(ExitCode.Success)
-
 }
 
 class Application[F[_]: ConcurrentEffect: Timer] {
